bd143a73ac8d6cb3d7ecebd3b9f95a251ea2b29d
duitermarkt <56364050+duitermarkt@users.noreply.github.com>
2019-10-09 15:34:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+
 window.onload = () => {
   'use strict';
-  
-  if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js');
-    }
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('/sw.js');
+    console.log('Service worker registered.');
+
   }
-  
+}
